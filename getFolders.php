8ebb963f0b3087075ee4a015a3b6d5c333c45c62
Alan Lansdowne <alan_lansdowne@yahoo.com>
2023-07-18 23:57:18 +0100

    Create getFolders.php

---
+++ getFolders.php
@@ -0,0 +1,33 @@
+function getFolders ($Target_Folder, $Folders_to_Skip = []) {
+
+  $ArrayOfFolderContents = [];
+
+  if (is_dir($Target_Folder)) {
+
+    $ArrayOfFolderContents['Files'] = [];
+
+    $assets = scandir($Target_Folder);
+
+    $Folders_to_Skip = array_merge(['.', '..'], $Folders_to_Skip);
+
+    foreach ($assets as $asset) {
+
+      if (!in_array($asset, $Folders_to_Skip)) {
+
+        if (filetype($Target_Folder.'/'.$asset) === 'dir') {
+
+          $ArrayOfFolderContents[$asset] = getFolders($Target_Folder.'/'.$asset);
+        }
+
+        else {
+
+          $ArrayOfFolderContents['Files'][] = $asset;
+        }
+      }
+    }
+
+    reset($assets);
+
+    return $ArrayOfFolderContents;
+  }
+}
